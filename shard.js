1dd398ac0fc70818ca5acf4f37ca31b3f7e891e7
funnbot <unconfigured@null>
2017-02-07 05:59:30 -0800

    s

--- shard.js
+++ shard.js
@@ -1,5 +1,3 @@
-/* Require discord.js */
 const Discord = require('discord.js');
-/* Create a new manager and spawn 2 shards */
 const Manager = new Discord.ShardingManager('./bot.js');
 Manager.spawn(2);
