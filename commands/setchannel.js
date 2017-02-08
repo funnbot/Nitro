519eba940f78f7989640600b6d8f34b0b8da03cf
funnbot <unconfigured@null>
2017-02-07 17:55:11 -0800

    setchannel added to help  restartmusic (dev) command

---
+++ commands/setchannel.js
@@ -0,0 +1,13 @@
+exports.run = (message, bot, suffix, args) => {
+  
+}
+
+exports.conf = {
+  userPerm:[],
+  botPerm:["SEND_MESSAGES"],
+  coolDown:0,
+  dm:false,
+  category:"Music",
+  help:"Set the channel that next song messages are sent to.",
+  args:"",
+}
