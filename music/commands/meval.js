99921b51b6e48df84b9c190d8c2fb655d0d51b71
funnbot <unconfigured@null>
2017-02-23 09:14:42 -0800

    meval

--- music/commands/meval.js
+++ music/commands/meval.js
@@ -2,13 +2,13 @@ exports.run = (message, bot, suffix, args) => {
     if (message.author.id === "163735744995655680") {
 
         try {
-            let toeval = eval(message.suffix);
+            let toeval = eval(suffix);
             let inspect = util.inspect(toeval);
 
             if (inspect.length > 1900) {
                 inspect = inspect.substr(0, 1900);
             }
-            message.channel.sendMessage("**Input**\n" + message.suffix + "\n\n**Output**\n```js\n" + inspect + "```")
+            message.channel.sendMessage("**Input**\n" + suffix + "\n\n**Output**\n```js\n" + inspect + "```")
                 .then(m => m.delete(20000)).catch(console.log);
         } catch (err) {
             message.channel.sendMessage("**```prolog\nError:\n\n" + err + "```**")
