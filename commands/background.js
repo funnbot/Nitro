044827b2bcfa1362d2e7601cbf53fb1e8632920d
funnbot <unconfigured@null>
2017-03-06 07:49:39 -0800

    Such Many Images

--- commands/background.js
+++ commands/background.js
@@ -1,8 +1,16 @@
-const bg = require('../functions/bg');
+const bg = require('../functions/bg')
 
 exports.run = (message, bot, send) => {
     
-  if (!message.args[0]) return send("**The available backrounds are: `default`**")
+  if (!message.args[0]) return send("**The available backround categories are: `solid`, `nightsky`, and `cities` or set to `default`**")
+  if (!bg[message.args[0]]) return send("*Invalid category*\n\n**The available backround categories are: `solid`, `nightsky`, and `cities`**")
+
+  if (message.args[0] === "default") {
+    message.channel.sendMessage("**Your background reset to default**")
+    bot.profile.setBg(message.author.id, "default")
+  }
+
+  if (!message.args[1]) return message.channel.sendFile(bg[message.args[0]].file, "bg.png", "__**"+message.args[0]+"**__\n")
 
 }
 
--- functions/bg.js
+++ functions/bg.js
@@ -1,7 +1,19 @@
 module.exports = {
-    default:{
-        file:"./images/bg/blue.png",
-        price:0,
-        cat:"flat"
-    }
-} 
+
+  solid:{
+    file: "./images/bg/solid/solid.png",
+    fold: "./images/bg/solid"
+  },
+  nightsky: {
+    file: "./images/bg/nightsky/nightsky.png",
+    fold: "./images/bg/nigthsky"
+  },
+  cities: {
+    file: "./images/bg/cities/cities.png",
+    fold: "./images/bg/cities"
+  },
+  default: {
+      file: "./images/bg/default.png"
+  }
+
+}
