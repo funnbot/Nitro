90e1e432c28a7df36ee41e8e061a004fb236cae7
Funnbot <dillonbayless1@gmail.com>
2017-03-07 09:34:18 -0800

    Update changes.js

--- commands/changes.js
+++ commands/changes.js
@@ -1,1 +1,13 @@
+exports.run = (message, bot) => {
+  message.send("Pong! "+bot.ping+"ms")
+}
 
+exports.conf = {
+  userPerm:[],
+  botPerm:["SEND_MESSAGES"],
+  coolDown:0,
+  dm:true,
+  category:"Other",
+  help:"Ping! Pong!",
+  args:""
+}
