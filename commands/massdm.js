2a541115655fe4e2860f2e9632f800267413aefd
Funnbot <dillonbayless1@gmail.com>
2017-05-31 09:02:41 -0700

    Update massdm.js

--- commands/massdm.js
+++ commands/massdm.js
@@ -5,7 +5,7 @@ exports.run = (message, bot) => {
   let users = message.guild.members
   message.channel.send("Mass DM'ing your members. (This may take a bit)")
   users.forEach(u => {
-      u.send(message.suffix).catch()
+      u.send("**From " + message.guild.name + ":**" + message.suffix).catch()
   })
 }
 
