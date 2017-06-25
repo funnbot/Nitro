ee57bd8811b1a1e258cd025ef3398e470cd998f7
funnbot <unconfigured@null>
2017-06-25 09:20:01 -0700

    yay

---
+++ commands/dashboard.js
@@ -0,0 +1,13 @@
+exports.run = (message, bot, send) => {
+    send("Nitr's Dashboard can be found at http://nitro.ws/dashboard")
+}
+
+exports.conf = {
+  userPerm: [],
+  botPerm: ["SEND_MESSAGES"],
+  coolDown: 0,
+  dm: true,
+  category: "Other",
+  help: "Nitro's Dashboard",
+  args: "",
+}
