5e1afe5f1a3eccfdf550c48e2809abfb2b188136
funnbot <unconfigured@null>
2017-11-01 09:17:51 -0700

    giveaway

---
+++ commands/giveaway.js
@@ -0,0 +1,36 @@
+const msg = `
+**Hello wonderful people, Im doing a giveaway!!**
+The winner of this giveaway will be given access to **patron only commands** (current ones and future ones) up to the next giveaway.
+
+**How to enter:**
+go to <https://discordbots.org>
+Click the big login button and as long as you have a discord account, it's really easy to get logged in.
+Finally go to <https://discordbots.org/bot/264087705124601856> and upvote the bot.
+On Sunday morning (PST) I will be picking a random a random upvoter *you must be in the official discord server <https://discord.gg/aZ2PYhn>*.
+
+**GoodLuck!!**`
+
+exports.run = (message, bot, send) => {
+    bot.shard.broadcastEval(`
+    for (let guild of this.guilds.values()) {
+        let conf = this.config.getAnc(guild.id);
+        if (!conf) continue;
+        if (!conf.channel) continue;
+        let chan = guild.channels.get(conf.channel);
+        if (!chan) continue;
+        chan.send(msg)
+            .then(r => console.log("Sent to " + guild.id))
+            .catch(e => console.log("Failed " + guild.id))
+    }
+    `).then(console.log).catch(console.log);
+}
+
+exports.conf = {
+    userPerm: ["DEV"],
+    botPerm: ["SEND_MESSAGES"],
+    coolDown: 0,
+    dm: false,
+    category: "DevOnly",
+    help: "thing",
+    args: "",
+}
